12a33069043f826a67869a046d79447c32495b12
Jonathan Orrego Montoya <jonathan.orrego@ucp.edu.co>
2025-03-16 22:23:40 -0500

    cambios cierre horas gratis

--- src/containers/Login/Login.test.tsx
+++ src/containers/Login/Login.test.tsx
@@ -1,17 +1,72 @@
-import { describe, it, expect } from 'vitest';
-import { render } from '@testing-library/react';
+import { describe, it, expect, vi, Mock } from 'vitest';
+import { fireEvent, render, screen, act, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom'
+import { getAuth } from '../../services/getAuth';
 import { SessionProvider } from '../../context/AuthContext';
 import { Login } from './Login';
 
+vi.mock('react-router-dom', async () => {
+    const actual = await vi.importActual('react-router-dom');
+    return {
+        ...actual,
+        useNavigate: () => mockNavigate
+    }
+});
+
+vi.mock('../../services/getAuth', () => ({
+    getAuth: vi.fn(),
+}));
+
+const mockNavigate = vi.fn();
+const mockGetAuth = getAuth as Mock;
+
 describe('<Login />', () => {
-    it('deberia renderizar el login', () => {
-        render(
+    const handleLogin = () => {
+        return render(        
         <SessionProvider>
                 <MemoryRouter>
                         <Login />
                 </MemoryRouter>
-            </SessionProvider>
-        );
+            </SessionProvider>)
+        }
+
+    it('deberia renderizar el login', async () => {
+        mockGetAuth.mockRejectedValue(new Error("Invalid credentials"));
+        handleLogin();
+        // render(
+        // <SessionProvider>
+        //         <MemoryRouter>
+        //                 <Login />
+        //         </MemoryRouter>
+        //     </SessionProvider>
+        // );
+        const usernameInput = screen.getByPlaceholderText('Username');
+        const passwordInput = screen.getByPlaceholderText('Password');
+        const buttonLogin = screen.getByRole('button', { name: 'Login' });
+        await act(() => {
+            fireEvent.change(usernameInput, { target: { value: 'WrongUser' } });
+            fireEvent.change(passwordInput, { target: { value: 'WrongPassword' } });
+            fireEvent.click(buttonLogin);
+        });
+        const errorMessage = screen.getByText('Invalid credentials');
+        expect(errorMessage).toBeInTheDocument();
+    })
+
+    it('deberia rederigir el login', async () => {
+        mockGetAuth.mockResolvedValue({});
+        handleLogin()
+
+        const usernameInput = screen.getByPlaceholderText('Username');
+        const passwordInput = screen.getByPlaceholderText('Password');
+        const buttonLogin = screen.getByRole('button', { name: 'Login' });
+        await act(() => {
+            fireEvent.change(usernameInput, { target: { value: 'validUser' } });
+            fireEvent.change(passwordInput, { target: { value: 'validPassword' } });
+            fireEvent.click(buttonLogin);
+        });
+        await waitFor(() => {
+            expect(mockGetAuth).toHaveBeenCalledWith('validUser', 'validPassword');
+            expect(mockNavigate).toHaveBeenCalledWith('/orders');
+        })
     })
 })
